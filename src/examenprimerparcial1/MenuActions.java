1a1298d36a86086e3533b974f5fb72af8da262cd
RicherVG <166271618+RicherVG@users.noreply.github.com>
2026-02-20 08:58:34 -0600

    correccion de ubicacion de archivos

---
+++ src/examenprimerparcial1/MenuActions.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenprimerparcial1;
+
+/**
+ *
+ * @author riche
+ */
+
+public interface MenuActions {
+    void submenu();
+    void ejecutarOpcion(int opcion);
+}
